77397ebc25279a2d066f2b702ca3ef5828140819
Sandro <moneyrunstheworld176@gmail.com>
2023-06-26 23:34:51 +0800

    Update app.js

--- CALCULATOR/app.js
+++ CALCULATOR/app.js
@@ -1,31 +1,59 @@
 let display = document.getElementById('display');
 
 let buttons = Array.from(document.getElementsByClassName('button'));
+let operators = ['+', '-', '*', '/'];
+let lastInput = '';
 
-buttons.map( button => {
-    button.addEventListener('click', (e) => {
-        switch(e.target.innerText){
-            case 'C':
-                display.innerText = '';
-                break;
+buttons.map(button => {
+  button.addEventListener('click', (e) => {
+    let buttonText = e.target.innerText;
 
-            case '←':
-                if(display.innerText){
-                    display.innerText = display.innerText.slice(0,-1);
-                }
-                break;
+    switch (buttonText) {
+      case 'C':
+        display.innerText = '0';
+        break;
 
-            case '=':
-                try {
-                    display.innerText = eval(display.innerText);                
-                } catch {
-                    display.innerText = 'SYNTAX ERROR!'
-                }
-
-                break;
-                
-            default:
-                display.innerText += e.target.innerText;
+      case '←':
+        if (display.innerText) {
+          display.innerText = display.innerText.slice(0, -1);
         }
-    });
+        break;
+
+      case '=':
+        try {
+          display.innerText = eval(display.innerText);
+        } catch {
+          display.innerText = 'SYNTAX ERROR!';
+        }
+        break;
+
+      case '.':
+        if (!display.innerText.includes('.') && !lastInput.includes('.')) {
+          display.innerText += buttonText;
+        }
+        break;
+
+      default:
+        if (
+          (display.innerText === '0' && buttonText !== '0') ||
+          (!operators.includes(buttonText) &&
+            display.innerText === '0' &&
+            buttonText === '0')
+        ) {
+          display.innerText = buttonText;
+        } else {
+          if (
+            operators.includes(buttonText) &&
+            operators.includes(lastInput) &&
+            lastInput !== '-'
+          ) {
+            display.innerText = display.innerText.slice(0, -1) + buttonText;
+          } else {
+            display.innerText += buttonText;
+          }
+        }
+    }
+
+    lastInput = buttonText;
+  });
 });
