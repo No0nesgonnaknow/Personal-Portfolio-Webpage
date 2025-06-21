67aa27e9104d2a4e90458f5036ed9160ebc949b3
Son Oller Panganiban <moneyrunstheworld176@gmail.com>
2025-06-22 02:19:13 +0800

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const aboutButton = document.querySelector('#about-button')
-const aboutButtonContent = document.querySelector('#about-button div')
-
-aboutButton.addEventListener('click', toggleButton)
-
-function toggleButton() {
-    aboutButtonContent.classList.toggle('loading')
-}
